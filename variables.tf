222440cee928dd74e2d2d98a92cfcf20190ded13
gurugreen <guruprasanna.green@gmail.com>
2025-01-14 23:35:28 +0530

    updated credentials

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "AWS region where the bucket will be created"
   type        = string
 }
+
+variable "access_key" {
+  description = "AWS region where the bucket will be created"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS region where the bucket will be created"
+  type        = string
+}
