47d7c602a7f0e03028ad2e246edf7460093802f3
gurugreen <guruprasanna.green@gmail.com>
2025-01-15 01:51:55 +0530

    fix

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_lambda_permission" "example_lambda_permission" {
 # Deploy the API Gateway
 resource "aws_api_gateway_deployment" "example_deployment" {
   rest_api_id = aws_api_gateway_rest_api.example_api.id
-  stage_name  = "Demo"
+  # stage_name  = "Demo"
 
   depends_on = [
     aws_api_gateway_integration.example_integration,
