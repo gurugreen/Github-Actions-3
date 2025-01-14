2e2034ffe693bc080ac17a812c00c42f81ebeb2a
gurugreen <guruprasanna.green@gmail.com>
2025-01-14 23:44:00 +0530

    update

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.aws_region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 terraform {
